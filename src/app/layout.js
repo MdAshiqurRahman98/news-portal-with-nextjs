cacc11ee66dc6f2fe266313a453fc1734bec9a9c
MdAshiqurRahman98 <MDASHIQUROFFICIAL@gmail.com>
2024-01-06 21:23:25 +0600

    Header added

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,10 +14,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar></Navbar>
-        {children}
+        <div className='min-h-screen'>
+          <Navbar></Navbar>
+          {children}
+        </div>
         <Footer></Footer>
       </body>
     </html>
-  )
+  );
 }
--- src/components/shared/Footer.js
+++ src/components/shared/Footer.js
@@ -1,11 +1,83 @@
-import { Box, Container } from '@mui/material';
+import {
+    Box,
+    Button,
+    Container,
+    IconButton,
+    Stack,
+    Typography,
+} from "@mui/material";
+
+// Icons
+import FacebookIcon from "@mui/icons-material/Facebook";
+import Link from "next/link";
 
 const Footer = () => {
+    const navItems = [
+        {
+            route: "Home",
+            pathname: "/",
+        },
+        {
+            route: "Pages",
+            pathname: "/pages",
+        },
+        {
+            route: "Category",
+            pathname: "/category",
+        },
+        {
+            route: "News",
+            pathname: "/news",
+        },
+        {
+            route: "About",
+            pathname: "/about",
+        },
+        {
+            route: "Contact",
+            pathname: "/contact",
+        },
+    ];
+
     return (
-        <Box className='bg-black px-2 py-10'>
-            <Container></Container>
+        <Box className="bg-black px-2 py-10">
+            <Container>
+                <Box
+                    className="w-full text-center"
+                    sx={{
+                        "& svg": {
+                            color: "white",
+                        },
+                    }}
+                >
+                    <IconButton>
+                        <FacebookIcon />
+                    </IconButton>
+                    <IconButton>
+                        <FacebookIcon />
+                    </IconButton>
+                    <IconButton>
+                        <FacebookIcon />
+                    </IconButton>
+                    <IconButton>
+                        <FacebookIcon />
+                    </IconButton>
+                </Box>
+
+                <Box className="w-full text-center">
+                    {navItems.map((item) => (
+                        <Link key={item} href={item.pathname}>
+                            <Button className="text-white">{item.route}</Button>
+                        </Link>
+                    ))}
+                </Box>
+
+                <Typography variant="body2" color="gray" textAlign="center">
+                    Copyright &copy; {new Date().getUTCFullYear()} The Dragon News | All Rights Reserved.
+                </Typography>
+            </Container>
         </Box>
     );
 };
 
-export default Footer;
+export default Footer;  
