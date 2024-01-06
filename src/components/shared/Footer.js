43ca2ec4c10b0bf8b250fca07183565f59f5b572
MdAshiqurRahman98 <MDASHIQUROFFICIAL@gmail.com>
2024-01-06 20:15:31 +0600

    assets added

---
+++ src/components/shared/Footer.js
@@ -0,0 +1,11 @@
+import { Box, Container } from '@mui/material';
+
+const Footer = () => {
+    return (
+        <Box className='bg-black px-2 py-10'>
+            <Container></Container>
+        </Box>
+    );
+};
+
+export default Footer;
