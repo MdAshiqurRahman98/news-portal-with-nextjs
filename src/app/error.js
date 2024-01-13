646eae7f79d4d8201c8ea2312427aead044fcb6a
MdAshiqurRahman98 <MDASHIQUROFFICIAL@gmail.com>
2024-01-13 20:16:14 +0600

    error page added

---
+++ src/app/error.js
@@ -0,0 +1,13 @@
+'use client';
+
+const ErrorPage = () => {
+    return (
+        <div className="mt-5">
+            <h1 className="text-2xl text-red-500 text-center">
+                Something went wrong!
+            </h1>
+        </div>
+    );
+};
+
+export default ErrorPage;
